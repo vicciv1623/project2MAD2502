a714f502b238a10ace0aea1a54689814325e5d1b
vicciv1623 <143960937+vicciv1623@users.noreply.github.com>
2025-03-15 10:04:20 -0400

    Update julia.py

--- julia.py
+++ julia.py
@@ -1,9 +1,13 @@
+import numpy as np
+
 def get_julia(c:complex, max_iterations:int, z:complex) -> int | None:
   """
   Returns the number of iterations before the complex number following the
   Julia sequence exceeds a magnitude of c
 
-  Input: c for the complex number calling the Julia set, max_iterations determining number of steps until to stop, and z for the complex point to test
+  Input: c for the complex number calling the Julia set
+         max_iterations determining number of steps until to stop
+         z for the complex point to test
   Output: int value for number of iterations until complex number magnitude is greater than the bigger value between 2 and absolute value of c or
             None if magnitude is lower or equal to the bigger value between 2 and absolute value of c even after completing max_iterations
   """
@@ -24,13 +28,17 @@ def get_julia_color_arr(z_arr: np.ndarray, c:complex, max_iterations: int) -> np
   output: ndarray which holds the float values which when converted to gray scale will generate
           a Julia image"""
   whole_grid = np.zeros(z_arr.shape)
-  row, col = whole_grid.shape
-  for i in range(row):
-    for j in range(col):
-      escape = get_julia(c, max_iterations, z_arr[i,j])
-      if escape is None:
-        whole_grid[i,j] = 0
+  z=np.zeros(z_arr.shape,dtype=complex)  #beginning of sequence
+  not_escaped=np.ones(z_arr.shape,dtype=bool)
+  for i in range(max_iterations):
+      if i == 0:
+        z[not_escaped] = z_arr[not_escaped]  #first term in sequence
       else:
-        whole_grid[i,j] = (max_iterations-escape+1) / (max_iterations+1)
+        z[not_escaped] = z[not_escaped]**2 + c    #nth term in sequence
+      if_escaped = (np.abs(z) > max(np.abs(c),2)) & not_escaped  
+      whole_grid[if_escaped] = i + 1  #updating grid where it escapes
+      not_escaped = not_escaped & (if_escaped == False)
+  whole_grid[not_escaped] = max_iterations + 1
+  color_grid = (max_iterations - whole_grid + 1) / (max_iterations + 1)
 
-  return whole_grid
+  return color_grid
