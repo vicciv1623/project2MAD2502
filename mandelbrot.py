f9a276c074ef08c427ea3a50b844a219e1b7a4cf
Russellliao <dongdongliao@ufl.edu>
2025-03-15 00:15:29 -0400

    Create mandelbrot.py
    updated get_escape_time_color_arr function

---
+++ mandelbrot.py
@@ -0,0 +1,57 @@
+def get_escape_time(c:complex, max_iterations:int) -> int | None:
+  """
+  Returns the number of iterations before the complex number following the
+  Mandelbrot sequence exceeds a magnitude of 2
+
+  Input: c for starting complex number, and max_iterations determining number of steps until to stop
+  Output: int value for number of iterations until complex number magnituded is greater than 2 or
+            None if magnitude is lower or equal to 2 even after completing max_iterations
+  """
+  z=c
+  if abs(z) > 2:
+    return 0
+  for i in range(max_iterations):
+    z=z**2 + c
+    if abs(z) > 2:
+      return i+1
+  return None
+  
+def get_complex_grid(top_left:complex, bottom_right:complex, step:float) -> np.ndarray:
+  """returns a grid of complex numbers as coordinates, each by a distance of [step]
+
+  input: top_left which is the "beginning" complex number that will located in the top left corner
+         bottom_right: which is a complex that specifies the end points of the grid laterally and longitudinally
+         step: which is an int that indicates the length of a unit within the grid
+  output: an ndarray with complex numbers distanced by a specific distance (step)"""
+  width_dist = bottom_right.real - top_left.real
+  height_dist = top_left.imag - bottom_right.imag
+
+  width = int(width_dist // step)
+  height = int(height_dist // step)
+
+  width_side = np.arange(0,width*step+step, step)
+  height_side = np.arange(0,height*step+step, step, dtype="complex128")
+  height_side *= complex(0,-1)
+
+  grid = width_side + height_side.reshape(height+1,1)
+  grid += top_left
+  return grid
+
+def get_escape_time_color_arr(c_arr: np.ndarray, max_iterations: int) -> np.ndarray:
+  """"returns an nd array with float values pertaining to the escape values of c_arr
+  input: c_arr which is the complex number grid used to generate mandelbrot image
+         max_iterations is the int parameter used for calculating the escape time
+  output: ndarray which holds the float values which when converted to gray scale will generate
+          a mandelbrot image"""
+  whole_grid = np.zeros(c_arr.shape)
+  z=np.zeros(c_arr.shape,dtype=complex)
+  not_escaped=np.ones(c_arr.shape,dtype=bool)
+  for i in range(max_iterations):
+      z[not_escaped] = z[not_escaped]**2 + c_arr[not_escaped]
+      if_escaped = (np.abs(z) > 2) & not_escaped
+      whole_grid[if_escaped] = i + 1
+      not_escaped = not_escaped & (if_escaped == False)
+  whole_grid[not_escaped] = max_iterations + 1
+  color_grid = (max_iterations - whole_grid + 1) / (max_iterations + 1)
+
+  return color_grid
